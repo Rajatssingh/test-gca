f7cba8fcf2faa7a6101b916b60deec978c09c322
Rajat Singh <rajatssingh@google.com>
2025-04-30 01:13:02 +0530

    test-x"

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
 print("hello World")
+
+# write code for binary search
+def binary_search(arr, target):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
